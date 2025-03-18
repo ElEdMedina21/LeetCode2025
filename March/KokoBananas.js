a0f516772182b949cb1ae625af67044405e400c2
Elias Medina <emedina21@alumnos.uaq.mx>
2025-03-18 13:27:53 -0600

    March 18, must revisit

---
+++ March/KokoBananas.js
@@ -0,0 +1,22 @@
+var minEatingSpeed = function(piles, h) {
+    let bot = 1; 
+    let top = Math.max(...piles);
+    let result = top
+    while(top >= bot){
+        let mid = Math.floor((bot+top)/2)
+        let currentHours = 0;
+        piles.forEach((pile)=>{
+            currentHours += Math.ceil(pile/mid)
+        })
+        if(currentHours <= h){
+            if(mid < result) result = mid;
+            top = mid-1
+        }
+        else{
+            bot = mid+1
+        }
+    }
+    return result
+};
+
+console.log(minEatingSpeed([312884470],312884469))
